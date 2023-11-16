b2935543866212ba56bad2580eefbdc13f0d15ad
郑体鹏 <chengs.love@163.com>
2023-11-16 12:51:41 +0800

    update

---
+++ c/easy/1720. Decode XORed Array.cpp
@@ -0,0 +1,40 @@
+/* 1720. Decode XORed Array
+ * There is a hidden integer array arr that consists of n non-negative integers.
+ * It was encoded into another integer array encoded of length n - 1,
+ * such that encoded[i] = arr[i] XOR arr[i + 1].
+ * For example, if arr = [1,0,2,1], then encoded = [1,2,3].
+ * You are given the encoded array. You are also given an integer first,
+ * that is the first element of arr, i.e. arr[0].
+ * Return the original array arr. It can be proved that the answer exists and is unique.
+ *
+ * Example 1:
+ * Input: encoded = [1,2,3], first = 1
+ * Output: [1,0,2,1]
+ * Explanation: If arr = [1,0,2,1], then first = 1 and
+ * encoded = [1 XOR 0, 0 XOR 2, 2 XOR 1] = [1,2,3]
+ *
+ * Example 2:
+ * Input: encoded = [6,2,7,3], first = 4
+ * Output: [4,2,0,7,4]
+ *
+ * Constraints:
+ * 2 <= n <= 10^4
+ * encoded.length == n - 1
+ * 0 <= encoded[i] <= 10^5
+ * 0 <= first <= 10^5
+ */
+
+class Solution {
+public:
+    vector<int> decode(vector<int> &encoded, int first) {
+        vector<int> arr(encoded.size() + 1);
+        arr[0] = first;
+        for (int i = 0; i < encoded.size(); i++) {
+            arr[i + 1] = arr[i] ^ encoded[i];
+        }
+        return arr;
+    }
+};
+
+// 时间8 ms，击败100%
+// 内存24.5 MB，击败74.91%
